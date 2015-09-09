21fe2ec14c52f8619947bdcccf5c58835ae837cc
Keerthikan <kerat12@student.sdu.dk>
2015-09-09 22:00:06 +0200

    resterende

---
+++ Lektion02/ROVI01/build/CMakeFiles/ROVI01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROVI01.dir/ROVI01.cpp.o"
+  "ROVI01.pdb"
+  "ROVI01"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ROVI01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
