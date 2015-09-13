54f386c18ce87d2564b368ae2f7c0f74b6dc90ad
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-09-13 19:52:06 +0200

    Tilføjet info om hvorfor Rotations matrix i translation er Identitetsmatrixen

--- Lektion02/Teoretisk/lec2.m
+++ Lektion02/Teoretisk/lec2.m
@@ -132,10 +132,7 @@ Tcb = [ Rcb,oC'-oB';
 Tca = Tba*Tcb
 
 
-
-
-
-
-
-
-
+z = 0
+y = 0
+x = 0
+Rrpy =[cos(z) -sin(z) 0; sin(z) cos(z) 0; 0 0 1]* [cos(y) 0 sin(y); 0 1 0; -sin(y) 0 cos(y)] * [1 0 0; 0 cos(x) -sin(x); 0 sin(x) cos(x)]
