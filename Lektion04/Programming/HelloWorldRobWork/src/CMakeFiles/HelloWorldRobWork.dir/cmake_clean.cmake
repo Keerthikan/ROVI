f2398ed1f67ae5ccba059f8f0b76e7b7561350db
Keerthikan <kerat12@student.sdu.dk>
2015-09-14 10:14:57 +0200

    Kiddi fus

---
+++ Lektion04/Programming/HelloWorldRobWork/src/CMakeFiles/HelloWorldRobWork.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HelloWorldRobWork.dir/HelloWorldRobWork.cpp.o"
+  "/home/student/ROVI/Lektion 01/HelloWorldRobWork/bin/Release/HelloWorldRobWork.pdb"
+  "/home/student/ROVI/Lektion 01/HelloWorldRobWork/bin/Release/HelloWorldRobWork"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HelloWorldRobWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
