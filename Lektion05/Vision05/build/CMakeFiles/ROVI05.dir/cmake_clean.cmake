319404d5be9cbee622f516739338c4cb59392d03
Keerthikan <kerat12@student.sdu.dk>
2015-09-19 20:58:35 +0200

    Vision02 opgave - hedder ROVI05.cpp

---
+++ Lektion05/Vision05/build/CMakeFiles/ROVI05.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROVI05.dir/ROVI05.cpp.o"
+  "ROVI05.pdb"
+  "ROVI05"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ROVI05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
