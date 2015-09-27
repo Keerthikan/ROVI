433d66cd2136978b073ff0bb30e9c2ced5b37565
Keerthikan <kerat12@student.sdu.dk>
2015-09-27 16:56:39 +0200

    VisionLektion06 prog. exercise samt teori

--- Lektion06/Vision06/src/ROVI06.cpp
+++ Lektion06/Vision06/src/ROVI06.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : ROVI01.cpp
-// Author      : Keerthikan R
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -14,40 +6,108 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <math.h>
 #include "opencv2/nonfree/nonfree.hpp"
-using namespace std;
 using namespace cv;
-
-
-int main() {
-
-    Mat image_full = imread("lena(1).bmp",CV_LOAD_IMAGE_GRAYSCALE);
-    Mat image_face = imread("lena_face.bmp",CV_LOAD_IMAGE_GRAYSCALE);
-    Mat image_hair = imread("lena_hair.bmp",CV_LOAD_IMAGE_GRAYSCALE);
-    Mat image_hat = imread("lena_hat.bmp",CV_LOAD_IMAGE_GRAYSCALE);
-
-    if(image_full.data < 0 && image_face.data < 0  && image_hair.data < 0 && image_hat.data < 0)
-    {
-        cout << "Not loaded" << endl;
-        exit(0);
-    }
-    imshow("image_full",image_full);
-    imshow("image_face",image_face);
-    imshow("image_hair",image_hair);
-    imshow("image_hat",image_hat);
-    waitKey(0);
-    destroyAllWindows();
-    //---------------------------------------------------//
-    // Pad the image with borders to avoid wrap around errors using copyMakeBorders()
-    int m = getOptimalDFTSize(2*image_full.rows);
-    int n = getOptimalDFTSize(2*image_full.cols);
-    copyMakeBorder(image_full, image_full,  0 , 0 ,0, n/2 , BORDER_CONSTANT, Scalar::all(0));
-    imshow("image_full",image_full);
-    waitKey(0);
-    // I think it is done
-    //---------------------------------------------------//
-    // Copy the gray image into the first channel of a new 2-channel image of type Mat_<Vec2f>,
-    // e.g. using merge(). The second channel should be all zeros.
-
-
-    return 0;
+using namespace std;
+void dftshift(cv::Mat_<float>& magnitude) {
+   const int cx = magnitude.cols/2;
+   const int cy = magnitude.rows/2;
+   
+   cv::Mat_<float> tmp;
+   cv::Mat_<float> topLeft(magnitude, cv::Rect(0, 0, cx, cy));
+   cv::Mat_<float> topRight(magnitude, cv::Rect(cx, 0, cx, cy));
+   cv::Mat_<float> bottomLeft(magnitude, cv::Rect(0, cy, cx, cy));
+   cv::Mat_<float> bottomRight(magnitude, cv::Rect(cx, cy, cx, cy));
+   
+   topLeft.copyTo(tmp);
+   bottomRight.copyTo(topLeft);
+   tmp.copyTo(bottomRight);
+   
+   topRight.copyTo(tmp);
+   bottomLeft.copyTo(topRight);
+   tmp.copyTo(bottomLeft);
 }
+
+int main()
+{
+    //Exercise 1 
+
+     Mat_<float> Lena_full = imread("lena(1).bmp", CV_LOAD_IMAGE_GRAYSCALE);	
+     Mat_<float> Lena_face = imread("lena_face.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+     Mat_<float> Lena_hair = imread("lena_hair.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+     Mat_<float> Lena_hat  = imread("lena_hat.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+     if(Lena_full.data < 0 )
+     {
+	cout << "loading issues" << endl; 
+	return -1; 
+     }
+
+     waitKey(0);
+   //---------------------------------//
+   // Pad Borders //
+   int x = getOptimalDFTSize( Lena_full.rows );
+   int y = getOptimalDFTSize( Lena_full.cols );
+   copyMakeBorder(Lena_full, Lena_full, 0, (x - Lena_full.rows), 0, (y - Lena_full.cols), BORDER_CONSTANT, Scalar::all(0));
+   //imshow("show",Lena_full);
+   //waitKey(0);
+   //--------------------------------//
+   // DFT - performing		     //
+   cv::Mat_<float> imgs[] = {Lena_full.clone(), Mat::zeros(Lena_full.size(), CV_32F)};
+   cv::Mat_<cv::Vec2f> img_dft;
+   merge(imgs,2,img_dft);
+   dft(img_dft, img_dft);     
+   split(img_dft,imgs);	
+   cv::Mat_<float> magnitude, phase;
+   cartToPolar(imgs[0],imgs[1],magnitude,phase);
+   dftshift(magnitude);
+   //log(magnitude,magnitude);
+   //normalize(magnitude,magnitude,0,1,CV_MINMAX);
+   //imshow("magnitude",magnitude);
+   //waitKey(0);
+
+
+//----------------------------------------------------------------//
+//Exercise 2							  //
+
+   magnitude(cv::Rect(magnitude.cols/2 - 25, magnitude.rows/2 -25, 50, 50)) = 0.0f;
+   cv::Mat_<float> magnitude_del;
+   log(magnitude,magnitude_del);
+   dftshift(magnitude_del);
+
+   polarToCart(magnitude_del,phase,imgs[0],imgs[1]);
+
+   Mat image;
+   Mat imgout;
+   merge(imgs,2,image);
+
+   dft(image,imgout, DFT_INVERSE+DFT_SCALE+DFT_REAL_OUTPUT);
+
+
+   cv::normalize(Lena_full, Lena_full, 0.0, 1.0, CV_MINMAX);
+   cv::normalize(magnitude_del, magnitude_del, 0.0, 1.0, CV_MINMAX);
+   cv::normalize(phase, phase, 0.0, 1.0, CV_MINMAX);	
+   cv::normalize(imgout, imgout, 0.0, 1.0, CV_MINMAX);
+   cout << "done" << endl; 
+   cv::imshow("Output", imgout);
+   cv::imshow("Input", Lena_full);
+   cv::imshow("Magnitude", magnitude_del);
+   waitKey(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
