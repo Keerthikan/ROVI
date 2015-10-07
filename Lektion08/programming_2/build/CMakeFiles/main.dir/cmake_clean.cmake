31e5c4fd1f0a79dac1f9c53656ed63270665b0de
Keerthikan <kerat12@student.sdu.dk>
2015-10-07 13:34:53 +0200

    Hej jes

---
+++ Lektion08/programming_2/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "/home/student/ROVI/Lektion08/programming_2/bin/Release/main.pdb"
+  "/home/student/ROVI/Lektion08/programming_2/bin/Release/main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
