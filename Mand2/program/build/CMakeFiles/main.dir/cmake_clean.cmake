c98f81d3b0f2459eaa4e9c4bc1d52c03c02abbbd
Keerthikan <kerat12@student.sdu.dk>
2015-10-18 13:41:34 +0200

    txt kiddi

---
+++ Mand2/program/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "/home/student/ROVI/Mand2/program/bin/Release/main.pdb"
+  "/home/student/ROVI/Mand2/program/bin/Release/main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
