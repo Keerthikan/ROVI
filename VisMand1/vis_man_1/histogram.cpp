d552f686e47c9d5202a7991c2d59875278c260ac
Je11 <jesjepsen@gmail.com>
2015-10-23 10:14:05 +0200

    Fixed Hist

--- VisMand1/vis_man_1/histogram.cpp
+++ VisMand1/vis_man_1/histogram.cpp
@@ -4,41 +4,55 @@ using namespace cv;
 
 void hist(const std::string& filename, const std::string& path, const std::string& saveas)
 {
-    Mat gray = imread(filename,CV_LOAD_IMAGE_GRAYSCALE);
+    Mat src = imread(filename);
 
-    if(gray.data < 0){
+    if(src.data < 0){
         cout << "Not loaded" << endl;
     }
 
-    // Initialize parameters
-    int histSize = 256;    // bin size
-    float range[] = { 0, 255 };
-    const float *ranges[] = { range };
+    /// Separate the image in 3 places ( B, G and R )
+    vector<Mat> bgr_planes;
+    split( src, bgr_planes );
 
-    // Calculate histogram
-    MatND hist;
-    calcHist( &gray, 1, 0, Mat(), hist, 1, &histSize, ranges, true, false );
+    /// Establish the number of bins
+    int histSize = 256;
 
-    // Show the calculated histogram in command window
-    double total;
-    total = gray.rows * gray.cols;
-    for( int h = 0; h < histSize; h++ )
-    {
-        float binVal = hist.at<float>(h);
-    }
+    /// Set the ranges ( for B,G,R) )
+    float range[] = { 0, 256 } ;
+    const float* histRange = { range };
 
-    // Plot the histogram
+    bool uniform = true; bool accumulate = false;
+
+    Mat b_hist, g_hist, r_hist;
+
+    /// Compute the histograms:
+    calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+
+    // Draw the histograms for B, G and R
     int hist_w = 512; int hist_h = 400;
     int bin_w = cvRound( (double) hist_w/histSize );
 
-    Mat histImage( hist_h, hist_w, CV_8UC1, Scalar( 255,0,0) );
-    normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
 
+    /// Normalize the result to [ 0, histImage.rows ]
+    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+    /// Draw for each channel
     for( int i = 1; i < histSize; i++ )
     {
-        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(hist.at<float>(i-1)) ) ,
-              Point( bin_w*(i), hist_h - cvRound(hist.at<float>(i)) ),
-              Scalar( 0, 0, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+              Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+              Scalar( 255, 0, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+              Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+              Scalar( 0, 255, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+              Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+              Scalar( 0, 0, 255), 2, 8, 0  );
     }
 
     imwrite( path+saveas, histImage);
