726d99881cc8f6751dedb5855364c1bbf730aa38
Keerthikan <kerat12@student.sdu.dk>
2015-11-06 18:38:26 +0100

    lol

--- VisMand1/vis_man_1/ContraHarmonic.cpp
+++ VisMand1/vis_man_1/ContraHarmonic.cpp
@@ -4,13 +4,13 @@ void ContraHarmonic(Mat src, Mat dst, int kernel, double P)
 {
   cout << "got it" << endl;
     Mat temp = src.clone();
-    dst = src.clone();
-    cout << src.rows <<  " " << src.cols << endl; 
+    Mat dst11 = src.clone();
+    cout << src.rows <<  " " << src.cols << endl;
     copyMakeBorder(temp,temp,kernel-1,kernel-1,kernel-1,kernel-1,BORDER_CONSTANT,Scalar(0,0,0));
 
-    for(int col = kernel/2; col < temp.rows - kernel/2-1; col++)
+    for(int row = kernel/2; row < temp.rows - kernel/2-1; row++)
     {
-        for(int row = kernel/2; row < temp.cols - kernel/2-1; row++)
+        for(int col = kernel/2; col < temp.cols - kernel/2-1; col++)
         {
 
         double den=0,num=0;
@@ -18,17 +18,19 @@ void ContraHarmonic(Mat src, Mat dst, int kernel, double P)
           {
             for(int j = -(kernel/2) ; j <= (kernel/2); j++)
             {
-                den += pow(temp.at<uchar>(col+i,row+j),P);
-                num += pow(temp.at<uchar>(col+i,row+j),P+1);
+                den += pow(temp.at<double>(row+i,col+j),P);
+                num += pow(temp.at<double>(row+i,col+j),P+1);
                 cout <<"Row: "<<row+i << " " << "col: "<< col + j << endl;
             }
           }
 
         //cout << num/den << endl;
         double value = num/den;
-        dst.at<uchar>(col,row) = (uchar)value;
+        dst11.at<double>(row,col) = value;
       }
     }
     cout << "done " << endl;
-    dst.convertTo(dst,CV_8U);
+    dst11.convertTo(dst,CV_8U);
+    imshow("sadas",dst);
+    waitKey(0);
 }
