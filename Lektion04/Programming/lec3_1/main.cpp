167c2464731452a3ae6b0834b750e39fc1d98c53
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2015-11-06 18:55:45 +0100

    changes

--- Lektion04/Programming/lec3_1/main.cpp
+++ Lektion04/Programming/lec3_1/main.cpp
@@ -10,80 +10,80 @@ double ** rotation(double z,double y,double x)
 {
     double** euler = 0;
     euler = new double*[3];
-    
+
     for (int i = 0; i < 3; ++i)
     {
         euler[i] = new double[3];
     }
-    
+
     euler[0][0] = cos(y)*cos(z);
     euler[0][1] = sin(x)*sin(y)*cos(z)-cos(x)*sin(z);
     euler[0][2] = cos(x)*sin(y)*cos(z)-sin(x)*sin(z);
-    
+
     euler[1][0] = cos(y)*sin(z);
     euler[1][1] = sin(x)*sin(y)*sin(z)+cos(x)*cos(z);
     euler[1][2] = cos(x)*sin(y)*sin(z)-sin(x)*cos(z);
-    
+
     euler[2][0] = -sin(y);
     euler[2][1] = sin(x)*cos(y);
     euler[2][2] = cos(x)*cos(y);
-    
+
     return euler;
 }
 
-double ** rotationT(double q[3])
-{
-    double** euler = 0;
-    euler = new double*[4];
-    
-    for (int i = 0; i < 4; ++i)
-    {
-        euler[i] = new double[4];
-    }
-    
-    euler[0][0] = cos(q[1])*cos(q[1]);
-    euler[0][1] = sin(q[0])*sin(q[2])*cos(q[2])-cos(q[0])*sin(q[2]);
-    euler[0][2] = cos(q[0])*sin(q[1])*cos(q[2])-sin(q[0])*sin(q[2]);
-    
-    euler[1][0] = cos(q[1])*sin(q[2]);
-    euler[1][1] = sin(q[0])*sin(q[1])*sin(q[2])+cos(q[0])*cos(q[2]);
-    euler[1][2] = cos(q[0])*sin(q[1])*sin(q[2])-sin(q[0])*cos(q[2]);
-    
-    euler[2][0] = -sin(q[1]);
-    euler[2][1] = sin(q[0])*cos(q[1]);
-    euler[2][2] = cos(q[0])*cos(q[1]);
-    
-    euler[3][0] = 0;
-    euler[3][1] = 0;
-    euler[3][2] = 0;
-    euler[3][3] = 1;
-    
-    
-    for(int j = 0 ; j<4 ; j++ ){
-        for(int k = 0 ; k<4 ; k++ ){
-            cout << euler[j][k] << "\t";
-        }
-        cout << endl;
-    }
-    return euler;
-}
+// double ** rotationT(double q[3])
+// {
+//     double** euler = 0;
+//     euler = new double*[4];
+//
+//     for (int i = 0; i < 4; ++i)
+//     {
+//         euler[i] = new double[4];
+//     }
+//
+//     euler[0][0] = cos(q[1])*cos(q[1]);
+//     euler[0][1] = sin(q[0])*sin(q[2])*cos(q[2])-cos(q[0])*sin(q[2]);
+//     euler[0][2] = cos(q[0])*sin(q[1])*cos(q[2])-sin(q[0])*sin(q[2]);
+//
+//     euler[1][0] = cos(q[1])*sin(q[2]);
+//     euler[1][1] = sin(q[0])*sin(q[1])*sin(q[2])+cos(q[0])*cos(q[2]);
+//     euler[1][2] = cos(q[0])*sin(q[1])*sin(q[2])-sin(q[0])*cos(q[2]);
+//
+//     euler[2][0] = -sin(q[1]);
+//     euler[2][1] = sin(q[0])*cos(q[1]);
+//     euler[2][2] = cos(q[0])*cos(q[1]);
+//
+//     euler[3][0] = 0;
+//     euler[3][1] = 0;
+//     euler[3][2] = 0;
+//     euler[3][3] = 1;
+//
+//
+//     for(int j = 0 ; j<4 ; j++ ){
+//         for(int k = 0 ; k<4 ; k++ ){
+//             cout << euler[j][k] << "\t";
+//         }
+//         cout << endl;
+//     }
+//     return euler;
+// }
 
 void rotation2rpy(double R[3][3]){
     double pitch = 0, roll = 0, yaw = 0;
     if(R[2][0]==1){
         cout << "R[2][0]: " << R[2][0] << " Special sequence!!" << endl;
-        
+
         yaw = acos(R[0][0]);
         roll = acos(R[2][2]);
-        
+
         std::cout << yaw << "   " << roll << std::endl;
-        
+
         double tmp[3][3] = {0};
-        
+
         tmp[0][0] = cos(roll);
         tmp[1][1] = cos(roll)*cos(yaw);
         tmp[2][2] = cos(yaw);
-        
+
         for(int i = 0 ; i < 3 ;  i++){
             for(int j = 0 ; j < 3 ; j++){
                 std::cout << tmp[i][j] << "  ";
@@ -97,13 +97,13 @@ void rotation2rpy(double R[3][3]){
         pitch = asin(-R[2][0]);
         yaw = atan2(R[2][1],R[2][2]);
         roll = atan2(R[1][0],R[0][0]);
-        
+
         std::cout << roll << " " << pitch << " " << yaw << std::endl;
         std::cout << std::endl;
-        
+
         double ** tmp = rotation(roll,pitch,yaw);
-        
-        
+
+
         for(int i = 0 ; i < 3 ;  i++){
             for(int j = 0 ; j < 3 ; j++){
                 std::cout << tmp[i][j] << "  ";
@@ -117,29 +117,29 @@ double ** rotationTr(double z,double y,double x)
 {
     double** euler = 0;
     euler = new double*[4];
-    
+
     for (int i = 0; i < 4; ++i)
     {
         euler[i] = new double[4];
     }
-    
+
     euler[0][0] = cos(y)*cos(z);
     euler[0][1] = sin(x)*sin(y)*cos(z)-cos(x)*sin(z);
     euler[0][2] = cos(x)*sin(y)*cos(z)-sin(x)*sin(z);
-    
+
     euler[1][0] = cos(y)*sin(z);
     euler[1][1] = sin(x)*sin(y)*sin(z)+cos(x)*cos(z);
     euler[1][2] = cos(x)*sin(y)*sin(z)-sin(x)*cos(z);
-    
+
     euler[2][0] = -sin(y);
     euler[2][1] = sin(x)*cos(y);
     euler[2][2] = cos(x)*cos(y);
-    
+
     euler[3][0] = 0;
     euler[3][1] = 0;
     euler[3][2] = 0;
     euler[3][3] = 1;
-    
+
     return euler;
 }
 
@@ -148,12 +148,12 @@ double **  transformation(double q[3], vector<double>& RPY)
 {
     double** result = 0;
     result = new double*[4];
-    
+
     for (int i = 0; i < 4; ++i)
     {
         result[i] = new double[4];
     }
-    
+
     for(int amount = 0; amount < RPY.size(); amount = amount +3){
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
@@ -168,56 +168,64 @@ double **  transformation(double q[3], vector<double>& RPY)
     }
     return result
 }
+
+void Jacobian(double q[3])
+{
+
+
+
+
+}
 int main()
 {
     double pi = 3.14;
     /*
      double test[3][3];
      std::cout << std::endl;
-     
+
      float random_number = 0;
-     
+
      for(int i = 0 ; i < 3 ; i++){
      for(int j = 0 ; j < 3 ; j++){
      srand((unsigned)time(0));
      random_number = rand()%10000+1;
-     
+
      for(int l = 0 ; l < 1000000000/2 ; l++ ){
-     
+
      }
-     
+
      test[i][j] = random_number/10000;
      cout << random_number << "  ";
      }
      }
      std::cout << std::endl;
-     
+
      //test[2][0] = 1;
-     
+
      rotation2rpy(test);
      */
-    
+
     double testQ[3] = {1,2,3};
-    
+
     rotationT(testQ);
-    
+
     cout << "done" << endl;
     vector<double> RPY;
     float random_number = 0;
-    
+
     for(int j = 0 ; j < 9 ; j++){
         srand((unsigned)time(0));
         random_number = rand()%10000+1;
-        
+
         for(int l = 0 ; l < 1000000000/2 ; l++ ){
-            
+
         }
-        
+
         RPY.push_back(random_number/10000);
     }
     cout << "done" << endl;
     cout <<  RPY.size() << endl;
-    
-    
+
+
     return 0;
 }
