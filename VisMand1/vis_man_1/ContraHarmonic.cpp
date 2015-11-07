b858d278e57bca9ab6d4d13713a2600507175d53
Keerthikan <kerat12@student.sdu.dk>
2015-11-07 14:15:53 +0100

    Contrahamonic Working

--- VisMand1/vis_man_1/ContraHarmonic.cpp
+++ VisMand1/vis_man_1/ContraHarmonic.cpp
@@ -12,43 +12,44 @@ void ContraHarmonic(Mat src, Mat dst, int kernel, double P)
         for(int col = kernel/2; col < temp.cols - kernel/2-1; col++)
         {
 
-        double den=0,num=0;
+        double den=0,num=0,value=0;
           for(int i = -(kernel/2); i <= (kernel/2) ; i++)
           {
             for(int j = -(kernel/2) ; j <= (kernel/2); j++)
             {
-                den += pow(temp.at<double>(row+i,col+j),P);
-                num += pow(temp.at<double>(row+i,col+j),P+1);
+                den += pow(temp.at<uchar>(row+i,col+j),P);
+                num += pow(temp.at<uchar>(row+i,col+j),P+1);
                 //cout <<"Row: "<<row+i << " " << "col: "<< col + j << endl;
             }
           }
 
         //cout << num/den << endl;
-        double value = num/den;
-        if (isnan(value))
-        {
-          cout << num << " " << den << endl;
-
-          if(isinf(num) && isinf(den))
-          {
-            dst.at<double>(row,col) = 1;
-          }
-
-          else if (isinf(num))
-          {
-            dst.at<double>(row,col) = den;
-          }
-          else if (isinf(den))
-          {
-            dst.at<double>(row,col) = num;
-          }
-
-        }
-        else
-        {
-          //cout << "was not nan" << endl;
-          dst.at<double>(row,col) = value;
-        }
+        value = num/den;
+        //cout << "set" << endl;
+        // if (isnan(value))
+        // {
+        //   //cout << num << " " << den << endl;
+        //
+        //   if(isinf(num) && isinf(den))
+        //   {
+        //     dst.at<double>(row,col) = 1;
+        //   }
+        //
+        //   else if (isinf(num))
+        //   {
+        //     dst.at<double>(row,col) = den;
+        //   }
+        //   else if (isinf(den))
+        //   {
+        //     dst.at<double>(row,col) = num;
+        //   }
+        //
+        // }
+        // else
+        // {
+        //   //cout << "was not nan" << endl;
+           dst.at<uchar>(row,col) = value;
+        // }
       //  namedWindow("ad",WINDOW_NORMAL);
       //  imshow("ad",dst);
       }
